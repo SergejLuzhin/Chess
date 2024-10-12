f840633f974a1151bb3338b2545e283cd2d7adb9
Sergej <luzhins764@gmail.com>
2024-10-12 20:20:31 +0300

    added bishop collision

--- src/Bishop.java
+++ src/Bishop.java
@@ -11,7 +11,9 @@ public class Bishop extends ChessPiece{
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        return movesInBoundaries(toLine, toColumn) && correctBishopMove(line, column, toLine, toColumn);
+        return movesInBoundaries(toLine, toColumn) &&
+                correctBishopMove(line, column, toLine, toColumn) &&
+                isMovingWithoutCollision(chessBoard, line, column, toLine, toColumn);
     }
 
     private boolean correctBishopMove(int line, int column, int toLine, int toColumn){
@@ -20,6 +22,60 @@ public class Bishop extends ChessPiece{
 
     @Override
     public boolean isMovingWithoutCollision(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        int currentLine;
+        int currentColumn;
+        if (toLine > line && toColumn > column){
+            currentLine = line + 1;
+            currentColumn = column + 1;
+            while (currentLine < toLine && currentColumn < toColumn){
+                if (chessBoard.board[currentLine][currentColumn] != null) return false;
+                currentLine++;
+                currentColumn++;
+            }
+            if (chessBoard.board[toLine][toColumn] == null) return true;
+            else {
+                return !chessBoard.board[toLine][toColumn].getColor().equals(chessBoard.board[line][column].getColor());
+            }
+        }
+        else if (toLine < line && toColumn > column){
+            currentLine = line - 1;
+            currentColumn = column + 1;
+            while (currentLine > toLine && currentColumn < toColumn){
+                if (chessBoard.board[currentLine][currentColumn] != null) return false;
+                currentLine--;
+                currentColumn++;
+            }
+            if (chessBoard.board[toLine][toColumn] == null) return true;
+            else {
+                return !chessBoard.board[toLine][toColumn].getColor().equals(chessBoard.board[line][column].getColor());
+            }
+        }
+        else if (toLine < line && toColumn < column){
+            currentLine = line - 1;
+            currentColumn = column - 1;
+            while (currentLine > toLine && currentColumn > toColumn){
+                if (chessBoard.board[currentLine][currentColumn] != null) return false;
+                currentLine--;
+                currentColumn--;
+            }
+            if (chessBoard.board[toLine][toColumn] == null) return true;
+            else {
+                return !chessBoard.board[toLine][toColumn].getColor().equals(chessBoard.board[line][column].getColor());
+            }
+        }
+        else if (toLine > line && toColumn < column){
+            currentLine = line + 1;
+            currentColumn = column - 1;
+            while (currentLine < toLine && currentColumn > toColumn){
+                if (chessBoard.board[currentLine][currentColumn] != null) return false;
+                currentLine++;
+                currentColumn--;
+            }
+            if (chessBoard.board[toLine][toColumn] == null) return true;
+            else {
+                return !chessBoard.board[toLine][toColumn].getColor().equals(chessBoard.board[line][column].getColor());
+            }
+        }
         return false;
     }
 
