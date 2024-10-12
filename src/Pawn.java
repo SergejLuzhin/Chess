7b8fc3e0e11bab597be4ff6df9d5deb93fde0dc7
Sergej <luzhins764@gmail.com>
2024-10-12 23:02:08 +0300

    added eat to pawn

--- src/Pawn.java
+++ src/Pawn.java
@@ -20,21 +20,20 @@ public class Pawn extends ChessPiece {
         return ((color.equals("White") && line == 1 && column == toColumn && (line - toLine == -1 || line - toLine == -2)) ||
                 (color.equals("White") && column == toColumn && line - toLine == -1) ||
                 (color.equals("Black") && line == 6 && column == toColumn && (line - toLine == 1 || line - toLine == 2)) ||
-                (color.equals("Black") && column == toColumn && line - toLine == 1));
+                (color.equals("Black") && column == toColumn && line - toLine == 1)) ||
+                ((Math.abs(line - toLine) == 1) && (Math.abs(column - toColumn) == 1));
     }
 
     @Override
     public boolean isMovingWithoutCollision(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        if (Math.abs(line - toLine) == 2) {
-            if (chessBoard.board[(line + toLine) / 2][column] == null && chessBoard.board[toLine][toColumn] == null) {
-                return true;
-            } else if (chessBoard.board[(line + toLine) / 2][column] == null) {
-                return !(chessBoard.board[toLine][toColumn].getColor().equals(chessBoard.board[line][column].getColor()));
-            }
-        } else if ((Math.abs(line - toLine) == 1)) {
+        if (Math.abs(line - toLine) == 2 && column == toColumn) {
+            return chessBoard.board[(line + toLine) / 2][column] == null && chessBoard.board[toLine][toColumn] == null;
+        } else if ((Math.abs(line - toLine) == 1 && column == toColumn)) {
             if (chessBoard.board[toLine][toColumn] == null) {
                 return true;
             }
+        } else if ((Math.abs(line - toLine) == 1) && (Math.abs(column - toColumn) == 1)) {
+            return !(chessBoard.board[toLine][toColumn].getColor().equals(chessBoard.board[line][column].getColor()));
         }
         return false;
     }
